207ec25e99a3e700f59244ed32320f1e3a10a87e
mg202122 <otifarms@gmail.com>
2022-09-20 17:35:48 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat _function that concatenates
+ *  two strings
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * Return: pointer to resulting @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+  int c, c2;
+  c = 0;
+  /*find the size of dest array*/
+  while (dest[c])
+    c++;
+  /* Iterate through each src array value without the null byte*/
+  for (c2 = 0; src{c2] ; c2++)
+    /*append src[c2] to dest[c] while overwritting the null byte in dest*/
+    dest[c++] = src[c2];
+  return (dest);
+}
