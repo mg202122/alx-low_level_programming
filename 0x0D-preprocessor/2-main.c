e7877185155dc69d622530c7a6aa055f17d0685c
mg202122 <otifarms@gmail.com>
2022-10-11 16:48:06 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
+ * main - function to print the name of the file it was compiled from
  *
- * Return: always 0
-*/
-
+ * Return: 0 for success
+ */
 int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
