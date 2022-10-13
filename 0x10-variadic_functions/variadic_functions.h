85cdefee133cf4d73d42dfac539069e27b471936
mg202122 <otifarms@gmail.com>
2022-10-13 16:13:50 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct what_format - defines representing a data type
+ * @type: the data type
+ * @f: function that prints data type in correct fmt
+ */
