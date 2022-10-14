272e1b12c39a4e9be389bdf1c3626c9983290368
mg202122 <otifarms@gmail.com>
2022-10-13 18:01:41 -0700

    readme

--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,1 @@
-non-read
+non-empty
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-
-/**
- * struct what_format - defines representing a data type
- * @type: the data type
- * @f: function that prints data type in correct fmt
- */
