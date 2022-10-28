4bd2682082a63bb722472acf9f0a2417307186b5
mg202122 <otifarms@gmail.com>
2022-10-27 18:41:28 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int a = 0, c = 0;
+unsigned int result = 0;
+int a = 0, c = 0;
 
-	if (b == NULL || (*b != '0' && *b != '1'))
-		return (0);
-	for (a = 0; b[a] != '\0'; a++)
-		;
-	a--;
-	while (a >= 0)
-	{
-		if (b[a] != '0' && b[a] != '1')
-			return (0);
-		result = result + (b[a] - '0') * (1 << c);
-		a--;
-		c++;
-	}
-	return (result);
+if (b == NULL || (*b != 0 && *b != 1))
+return (0);
+for (a = 0; b[a] != 0; a++)
+;
+a--;
+while (a >= 0)
+{
+if (b[a] != 0 && b[a] != 1)
+return (0);
+result = result + (b[a] - 0) * (1 << c);
+a--;
+c++;
+}
+return (result);
 }
